6ac1fabd0a02fc8f65b88ab913132cd59b02a334
bao hoang <hoanggiabao15899@gmail.com>
2021-10-05 16:00:44 +0700

    add favorite list not found

--- src/Screen/Favorite/index.tsx
+++ src/Screen/Favorite/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
+import { useSelector } from 'react-redux'
 import HeaderNoBack from '../../Component/HeaderNoBack'
 import styles from './styles'
 
 
 export default function index() {
+    const data = useSelector(state => state.auth.user)
+    const { listResults } = data
+    console.log(listResults);    
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <HeaderNoBack title='My list' />
             <View style={styles.container}>
+                {listResults?.list?.length > 0 ? 
+                <View></View>
+                :
+                <Text style={styles.title}>Your favorite list is empty</Text>   
+                }
             </View>
         </SafeAreaView>
     )
--- src/Screen/Favorite/styles.tsx
+++ src/Screen/Favorite/styles.tsx
@@ -1,11 +1,19 @@
 import { StyleSheet } from "react-native";
-import { COLOR } from "../../GlobalStyle";
+import { COLOR, FONT } from "../../GlobalStyle";
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: COLOR.BACKGROUNG,
         flex: 1,
         paddingHorizontal:15
+    },
+    title:{
+        marginTop:10,
+        fontFamily:'Roboto-Bold',
+        fontSize:FONT.TITLE,
+        color:COLOR.PINK,
+        textAlign:'center',
+        textTransform:'uppercase'
     },
 }) 
 
